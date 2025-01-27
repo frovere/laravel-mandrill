b8051bac074a036cee8afdbb102188ca3d32a325
Felipe Rovere <felipe@lirmi.com>
2025-01-27 16:34:03 -0300

    feat(deps)!: add laravel v11 support

--- tests/ServiceProviderTest.php
+++ tests/ServiceProviderTest.php
@@ -2,19 +2,20 @@
 
 namespace Felrov\Drill\Tests;
 
+use PHPUnit\Framework\Attributes\Test;
 use Symfony\Component\Mailer\Bridge\Mailchimp\Transport\MandrillApiTransport;
 use Symfony\Component\Mailer\Bridge\Mailchimp\Transport\MandrillHttpTransport;
 
 class ServiceProviderTest extends TestCase
 {
-    /** @test */
+    #[Test]
     public function it_gets_mandrill_https_driver(): void
     {
         $this->app['config']->set('services.mandrill.scheme', 'https');
         $this->assertInstanceOf(MandrillHttpTransport::class, $this->app->make('mailer')->getSymfonyTransport());
     }
 
-    /** @test */
+    #[Test]
     public function it_gets_mandrill_api_driver(): void
     {
         $this->app['config']->set('services.mandrill.scheme', 'api');
